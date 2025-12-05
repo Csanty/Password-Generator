2ed497e6dcc6dabcba90db9282406a9dbde2a457
Crystal Santy <clsanty@gmail.com>
2025-12-04 20:04:20 -0500

    Update with final edits.

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Steps to generate passwords:
+// User clicks button
+// Get a random character from array and assign it to results bar
+// Get 14 more random characters from array and assign them to results bar
+// Repeat steps for second results bar
+
+const characters = ["A","B","C","D","E","F","G","H","I",
+    "J","K","L","M","N","O","P","Q","R","S","T","U","V",
+    "W","X","Y","Z","a","b","c","d","e","f","g","h","i",
+    "j","k","l","m","n","o","p","q","r","s","t","u","v",
+    "w","x","y","z", "0", "1", "2", "3", "4", "5", "6", 
+    "7", "8", "9","~","`","!","@","#","$","%","^","&",
+    "*","(",")","_","-","+","=","{","[","}","]",",","|",
+    ":",";","<",">",".","?","/"];
+    
+let firstPassword = document.getElementById("first-password");
+let secondPassword = document.getElementById("second-password");
+
+function genPassword () {
+    firstPassword.textContent = "";
+    secondPassword.textContent = "";
+
+    for (let i = 0; i < 15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        firstPassword.textContent += characters[randomIndex]
+    }
+    for (let i = 0; i < 15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        secondPassword.textContent += characters[randomIndex]
+    }
+}
